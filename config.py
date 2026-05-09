2412c6e24c1379e8eabbfb9bc074c05d8031ffe5
Jesús Chaguan <jchaguan99@gmail.com>
2026-05-09 18:44:34 +0200

    aaaa

--- config.py
+++ config.py
@@ -1,12 +1,19 @@
 import os
+from dotenv import load_dotenv
 
-# Las llaves se leerán del sistema operativo, no del archivo
+# Intentar cargar el archivo .env (para local)
+load_dotenv()
+
+# Ahora busca en el .env o en el sistema (para el servidor)
 API_KEY = os.getenv('BINANCE_API_KEY')
 API_SECRET = os.getenv('BINANCE_API_SECRET')
+
+# Si sigue sin encontrar nada, avisamos para evitar el error de Binance
+if not API_KEY or not API_SECRET:
+    print("⚠️ ADVERTENCIA: No se detectaron las llaves API. Revisa tu archivo .env")
 
 SYMBOL = 'BTCUSDT'
 TIMEFRAME = '1m'
 
-# Modo Sueño
 SLEEP_HOUR = 0
 SLEEP_MINUTE = 5
